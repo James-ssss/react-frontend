8a4b0fb4bb5d1af502e097e38941bc68faea4b06
Ivan <werwetsad@gmail.com>
2023-12-05 23:15:02 +0500

    Меняется состояние полей заявки и комментария

--- src/Components/CreateTask.js
+++ src/Components/CreateTask.js
@@ -1,101 +1,133 @@
 import React, { useState } from "react";
-import {
-  Navbar,
-  Nav,
-  Button,
-  NavLink,
-  Form,
-  Stack,
-  Col,
-  Row,
-} from "react-bootstrap";
-import { Link } from "react-router-dom";
-import NaviBar from "./Navibar";
+import { Button, Form, Stack, Col, Row } from "react-bootstrap";
 
-const order = {};
 
-export default function CreateTask() {
+
+const CreateTask = () => {
   const [forms, setForms] = useState([
-    <div style={{display: "flex", justifyContent: "center"}}>
-      <h1>Новая заявка</h1>
-      <br />
-    </div>,
+    {
+      resource: "",
+      quantity: "",
+    },
   ]);
 
+  const [comment, setComment] = useState("");
+
+  const handleInputChange = (index, fieldName, value) => {
+    const updatedForms = [...forms];
+    updatedForms[index][fieldName] = value;
+    setForms(updatedForms);
+  };
+
+  const handleInputChangeCommentary = (value) => {
+    setComment(value);
+  };
 
   const handleSubmit = (event) => {
     event.preventDefault();
-    // Ваша логика обработки отправки формы
+    console.log(forms);
+    console.log(comment);
+  };
+
+  const handleDelete = (index) => {
+    if (forms.length == 1) return;
+    const updatedForms = forms.filter((_, i) => i !== index);
+    setForms(updatedForms);
   };
 
   return (
     <>
+      <div style={{ display: "flex", justifyContent: "center" }}>
+        <h1>Новая заявка</h1>
+        <br />
+      </div>
       <Stack gap={2} className="col-md-5 mx-auto">
-        {forms}
+        {forms.map((form, index) => (
+          <div key={index}>
+            <div
+              style={{
+                backgroundColor: "lightblue",
+                padding: "10px",
+                borderRadius: "10px",
+              }}
+            >
+              <Row>
+                <Col md={9} lg={9}>
+                  <Form flexDirection="column" onSubmit={handleSubmit}>
+                    <Form.Group className="mb-3" controlId="formComment">
+                      <Form.Control
+                        type="text"
+                        placeholder="Ресурс"
+                        value={form.resource}
+                        onChange={(e) =>
+                          handleInputChange(index, "resource", e.target.value)
+                        }
+                      />
+                    </Form.Group>
+                  </Form>
+                </Col>
+                <Col md={3} lg={3}>
+                  <Form flexDirection="column" onSubmit={handleSubmit}>
+                    <Form.Group className="mb-3" controlId="formComment">
+                      <Form.Control
+                        type="text"
+                        placeholder="Количество"
+                        value={form.quantity}
+                        onChange={(e) =>
+                          handleInputChange(index, "quantity", e.target.value)
+                        }
+                      />
+                    </Form.Group>
+                  </Form>
+                </Col>
+                <Col
+                  md={12}
+                  lg={12}
+                  className="d-flex justify-content-center align-items-center"
+                >
+                  <Button
+                    variant="danger"
+                    type="button"
+                    style={{ width: "33%" }}
+                    onClick={() => handleDelete(index)}
+                  >
+                    Удалить
+                  </Button>
+                </Col>
+              </Row>
+            </div>
+            <br />
+          </div>
+        ))}
         <Button
           variant="primary"
           type="submit"
           onClick={() =>
             setForms([
               ...forms,
-              <div>
-                <div style={{ backgroundColor: "lightblue", padding: "10px", borderRadius: "10px" }}>
-                  <Row>
-                    <Col md={9} lg={9}>
-                      <Form
-                        flexDirection="column"
-                        flexGrowY={1}
-                        Form
-                        onSubmit={handleSubmit}
-                      >
-                        <Form.Group className="mb-3" controlId="formComment">
-                          <Form.Control type="text" placeholder="Ресурс" />
-                        </Form.Group>
-                      </Form>
-                    </Col>
-                    <Col md={3} lg={3}>
-                      <Form
-                        flexDirection="column"
-                        flexGrowY={1}
-                        Form
-                        onSubmit={handleSubmit}
-                      >
-                        <Form.Group className="mb-3" controlId="formComment">
-                          <Form.Control type="text" placeholder="Количество" />
-                        </Form.Group>
-                      </Form>
-                    </Col>
-                    <Col
-                      md={12}
-                      lg={12}
-                      className="d-flex justify-content-center align-items-center"
-                    >
-                      <Button
-                        variant="danger"
-                        type="submit"
-                        style={{ width: "33%" }}
-                      >
-                        Удалить
-                      </Button>
-                    </Col>
-                  </Row>
-                </div>
-                <br />
-              </div>,
+              {
+                resource: "",
+                quantity: "",
+              },
             ])
           }
         >
           + Добавить ресурс
         </Button>
         <br />
-        <Form flexDirection="column" flexGrowY={1} Form onSubmit={handleSubmit}>
+        <Form flexDirection="column" onSubmit={handleSubmit}>
           <Form.Group className="mb-3" controlId="formComment">
             <Form.Label>Комментарий к заявке</Form.Label>
-            <Form.Control type="text" placeholder="Комментарий" />
+            <Form.Control
+              type="text"
+              placeholder="Комментарий"
+              // Пример обработчика изменения введенного текста
+              onChange={(e) => handleInputChangeCommentary(e.target.value)}
+            />
           </Form.Group>
         </Form>
         <br />
-        <Button variant="success" type="submit">
+        <Button variant="success" type="submit" onClick={handleSubmit}>
           Подтвердить заявку
         </Button>
       </Stack>
@@ -103,4 +135,6 @@ export default function CreateTask() {
       <br />
     </>
   );
-}
+};
+
+export default CreateTask;
